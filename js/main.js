749ed174af4b68cbeeded1539cf45d83d8057dca
MingJingShu <53051141+MingJingShu@users.noreply.github.com>
2019-08-26 22:54:19 +0800

    把註解除掉
    網頁超奇怪><

--- js/main.js
+++ js/main.js
@@ -5,18 +5,16 @@ $('.Dogtitle').stop().addClass('in');
 
 //scrollspy
 $(document).scroll(function(e) {
-	/* Act on the event */
-	//console.log($(this).scrollTop());
+	
 	if($(this).stop().scrollTop() > 20){
-		//$('.my-icon-down').stop().animate({opacity: 0});
-		//$('.my-icon-down').stop().animate({display:none});
+	
 		$('.my-icon-down').addClass('down-now');
 		console.log($(this).scrollTop());
 
 	}
 	else{
 		$('.my-icon-down').removeClass('down-now');
-		//console.log($(this).scrollTop());
+		
 		console.log('removeNow');
 	}
 	});
