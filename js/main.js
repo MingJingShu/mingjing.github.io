d1937f3779556105eac9758a385dd1e49433ce7b
MingJingShu <53051141+MingJingShu@users.noreply.github.com>
2019-08-26 22:56:15 +0800

    關閉除錯
    測試中

--- js/main.js
+++ js/main.js
@@ -2,20 +2,19 @@ $(document).ready(function() {
 	console.log('ready');
 	//Dogtitle animation
 $('.Dogtitle').stop().addClass('in');
-
-//scrollspy
+	
 $(document).scroll(function(e) {
 	
 	if($(this).stop().scrollTop() > 20){
 	
 		$('.my-icon-down').addClass('down-now');
-		console.log($(this).scrollTop());
+		//console.log($(this).scrollTop());
 
 	}
 	else{
 		$('.my-icon-down').removeClass('down-now');
 		
-		console.log('removeNow');
+		//console.log('removeNow');
 	}
 	});
 
